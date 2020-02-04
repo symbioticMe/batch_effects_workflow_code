6713e9bd152b068e2ef073fecf71eb6f3c80447e
Jelena Chuklina <chuklina.jelena@gmail.com>
2020-02-04 13:53:04 +0100

    minor debug in correction of BE in PanCancer

--- PanCancer_study/analysis_PanCancer/3_correct_batch_effects.R
+++ PanCancer_study/analysis_PanCancer/3_correct_batch_effects.R
@@ -2,7 +2,7 @@ library(readr)
 library(proBatch)
 
 normalized_transitome_PanCancer = read_csv('data_PanCancer/2_interim_data/normalized_transitome_PanCancer.csv')
-sample_annotation_PanCancer = read_csv('data_PanCancer/3_data_for_plots/sample_annotation_PanCancer.csv') %>% as.data.frame()
+sample_annotation_PanCancer = read_csv('data_PanCancer/3_data_for_plots/sample_annotation_PanCancer.csv')
 
 corrected_transitome_proBatch = center_feature_batch_means_df(normalized_transitome_PanCancer, 
                                                               sample_annotation =  sample_annotation_PanCancer, 
