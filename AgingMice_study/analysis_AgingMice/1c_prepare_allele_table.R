a90d4d9062e6551b4b23f98f423b7d0558ea45c8
Jelena Chuklina <chuklina.jelena@gmail.com>
2020-02-07 15:04:18 +0100

    Updated allelle table prep

--- AgingMice_study/analysis_AgingMice/1c_prepare_allele_table.R
+++ AgingMice_study/analysis_AgingMice/1c_prepare_allele_table.R
@@ -1,13 +1,14 @@
 library(dplyr)
 library(readr)
+library(reshape2)
 
-Protein_Gene_LOCATION_AND_ALLELE_SYMBOL_v2 <- read.csv("~/Desktop/QTL_and_allele_heterozygosity/plot_intensity_w_allele_heterozygosity/Protein_Gene_LOCATION_AND_ALLELE_SYMBOL_v2.csv")
-peptide_df_AgingMice = read_csv("Aging_mice/data_Aging_mice/1_original_data_AgingMice/peptide_annotations_fixed.csv")
-sample_annotation_AgingMice = read_csv("Aging_mice/data_Aging_mice/2_interim_data_AgingMice//sample_annotation_Aging_mice.csv")
+Protein_Gene_allelle_mapping <- read.csv("data_AgingMice/1_original_data/Protein_Gene_LOCATION_AND_ALLELE_SYMBOL_v2.csv")
+peptide_df_AgingMice = read_csv("data_AgingMice/1_original_data/peptide_annotation.csv")
+sample_annotation_AgingMice = read_csv("data_AgingMice/3_data_for_plots/sample_annotation_AgingMice.csv")
 
 
-rownames(Protein_Gene_LOCATION_AND_ALLELE_SYMBOL_v2) = Protein_Gene_LOCATION_AND_ALLELE_SYMBOL_v2$Gene
-allelle_annotation_wide = Protein_Gene_LOCATION_AND_ALLELE_SYMBOL_v2[unique(peptide_df_AgingMice$Gene), 
+rownames(Protein_Gene_allelle_mapping) = Protein_Gene_allelle_mapping$Gene
+allelle_annotation_wide = Protein_Gene_allelle_mapping[unique(peptide_df_AgingMice$Gene), 
                                                                      c('Gene', setdiff(unique(sample_annotation_AgingMice$Strain), 'Mix'))]
 allelle_annotation_df = melt(data = allelle_annotation_wide, id.vars = 'Gene', measure.vars = names(allelle_annotation_wide)[-1])
 names(allelle_annotation_df)[2] = 'Strain'
@@ -16,4 +17,4 @@ allelle_annotation_df$allele = ifelse(allelle_annotation_df$value == 'red',
                                       ifelse(allelle_annotation_df$value == 'blue','C57BL6J', 'heterozygous'))
 
 
-write_csv(allelle_annotation_df, 'Aging_mice/data_Aging_mice/2_interim_data_AgingMice/allelle_annotation_df.csv')
+write_csv(allelle_annotation_df, 'data_AgingMice/3_data_for_plots/allelle_annotation_df.csv')
