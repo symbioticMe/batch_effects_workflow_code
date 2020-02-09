89ced971ee6b0c19335b55643e28c0ca2fcabbde
Jelena Chuklina <chuklina.jelena@gmail.com>
2020-02-09 13:36:41 +0100

    upd of normalize

--- AgingMice_study/analysis_AgingMice/2_quantile_normalize.R
+++ AgingMice_study/analysis_AgingMice/2_quantile_normalize.R
@@ -11,7 +11,8 @@ proteome_df_AgingMice = read_csv("data_AgingMice/2_interim_data/raw_proteome_Agi
 # quantile normalize of log2 transformed data matrix 
 normalized_df_AgingMice = normalize_data_df(proteome_df_AgingMice, 
                                             normalize_func = "quantile", 
-                                            log_base = NULL)
+                                            log_base = NULL,
+                                            keep_all = 'default')
 
 # save new data frame
 write_csv(normalized_df_AgingMice, 
@@ -21,7 +22,8 @@ write_csv(normalized_df_AgingMice,
 proteome_df_withRequants = read_csv('data_AgingMice/2_interim_data/raw_proteome_AgingMice_with_requants.csv')
 normalized_df_withRequants = normalize_data_df(proteome_df_withRequants, 
                                             normalize_func = "quantile", 
-                                            log_base = NULL)
+                                            log_base = NULL,
+                                            keep_all = 'default')
 
 # save new data frame
 write_csv(normalized_df_withRequants, 
