591fb306aae3bec7e17b0251c3d630b54a6baa1d
Patrick Pedrioli <pedrioli@gmail.com>
2020-06-05 09:30:25 +0000

    Initial commit

---
+++ fetch_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# InterLab
+wget -O InterLab_study/data_InterLab/1_original_data/all_sites_global_q_0.01_applied_to_local_global.txt ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2020/02/PXD004886/all_sites_global_q_0.01_applied_to_local_global.txt
+
+# AgingMice
+# TODO: Add after dataset becomes publicly availlable
