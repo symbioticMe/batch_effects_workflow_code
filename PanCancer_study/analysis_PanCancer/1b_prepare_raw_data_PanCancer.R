686ef1a0d646c81c0b307fe37635b125e29661dd
Patrick Pedrioli <pedrioli@gmail.com>
2020-06-05 09:44:56 +0000

    Parse gziped file

--- PanCancer_study/analysis_PanCancer/1b_prepare_raw_data_PanCancer.R
+++ PanCancer_study/analysis_PanCancer/1b_prepare_raw_data_PanCancer.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 
-raw_transitome_PanCancer = read_csv('data_PanCancer/1_original_data/feature_alignment_modB_trs.csv')
+raw_transitome_PanCancer = read_csv(gzfile('data_PanCancer/1_original_data/feature_alignment_modB_trs.csv.gz'))
 
 
 raw_transitome_PanCancer[raw_transitome_PanCancer$Intensity %in% 0,"Intensity"]<-NA
