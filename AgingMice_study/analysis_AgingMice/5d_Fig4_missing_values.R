a0f51a26199a497c5a843ddcb2ba4018bf75d002
Symbiotic Me <chuklina.jelena@gmail.com>
2020-09-13 18:29:57 +0200

    adjustments in Fig4

--- AgingMice_study/analysis_AgingMice/5d_Fig4_missing_values.R
+++ AgingMice_study/analysis_AgingMice/5d_Fig4_missing_values.R
@@ -4,6 +4,7 @@ library(readr)
 library(ggplot2)
 library(ggpubr)
 library(png)
+library(here)
 
 sample_annotation_AgingMice = read_csv("data_AgingMice/3_data_for_plots/sample_annotation_AgingMice.csv")
 normalized_df_withRequants = read_csv(file = "data_AgingMice/3_data_for_plots/normalized_proteome_AgingMice_with_requants.csv")
@@ -16,30 +17,33 @@ normalized_matrix_AgingMice = long_to_matrix(normalized_df_withRequants,
                                              sample_id_col = 'FullRunName', 
                                              qual_col = 'm_score', qual_value = 2)
 
-#selected annotation factors:
-factors_to_plot_AgingMice <- c("MS_batch",  "digestion_batch", "Diet", 'DateTime')
-breaks_for_heatmap = seq(8, 24, length.out = 101)
-
-n_samples = ncol(normalized_matrix_AgingMice)
-pep_completeness = apply(normalized_matrix_AgingMice, MARGIN = 1, FUN = function(x) sum(!is.na(x))/n_samples)
-peptides_75 = names(pep_completeness)[pep_completeness > .75 & pep_completeness]
-
-heatmap_normalized_AgingMice_75 <- plot_heatmap_diagnostic(normalized_matrix_AgingMice[peptides_75,], 
-                                                           sample_annotation = sample_annotation_AgingMice,
-                                                           factors_to_plot = factors_to_plot_AgingMice,
-                                                           fill_the_missing = -1,
-                                                           sample_id_col = 'FullRunName', 
-                                                           color_list = color_list_AgingMice,
-                                                           distance = "euclidean", method = "complete",
-                                                           breaks = c(-1, breaks_for_heatmap),
-                                                           show_rownames = F, show_colnames = F, 
-                                                           cluster_cols = T,
-                                                           legend = F,
-                                                           annotation_legend = F,
-                                                           plot_title = 'Peptides with >75% complete measurements')
-heatmap_normalized_AgingMice_75_p = heatmap_normalized_AgingMice_75$gtable
-ggsave(filename = 'plots_AgingMice/png_for_plots/heatmap_75_normalized_clustered.png', 
-       plot = heatmap_normalized_AgingMice_75_p, width = 7.18, height = 7.08, units = 'in')
+if (!'heatmap_75_normalized_clustered.png' %in% list.files('plots_AgingMice/png_for_plots')) {
+  #selected annotation factors:
+  factors_to_plot_AgingMice <- c("MS_batch",  "digestion_batch", "Diet", 'DateTime')
+  breaks_for_heatmap = seq(8, 24, length.out = 101)
+  
+  n_samples = ncol(normalized_matrix_AgingMice)
+  pep_completeness = apply(normalized_matrix_AgingMice, MARGIN = 1, FUN = function(x) sum(!is.na(x))/n_samples)
+  peptides_75 = names(pep_completeness)[pep_completeness > .75 & pep_completeness]
+  
+  heatmap_normalized_AgingMice_75 <- plot_heatmap_diagnostic(normalized_matrix_AgingMice[peptides_75,], 
+                                                             sample_annotation = sample_annotation_AgingMice,
+                                                             factors_to_plot = factors_to_plot_AgingMice,
+                                                             fill_the_missing = -1,
+                                                             sample_id_col = 'FullRunName', 
+                                                             color_list = color_list_AgingMice,
+                                                             distance = "euclidean", method = "complete",
+                                                             breaks = c(-1, breaks_for_heatmap),
+                                                             show_rownames = F, show_colnames = F, 
+                                                             cluster_cols = T,
+                                                             legend = F,
+                                                             annotation_legend = F,
+                                                             plot_title = 'Peptides with >75% complete measurements')
+  heatmap_normalized_AgingMice_75_p = heatmap_normalized_AgingMice_75$gtable
+  ggsave(filename = 'plots_AgingMice/png_for_plots/heatmap_75_normalized_clustered.png', 
+         plot = heatmap_normalized_AgingMice_75_p, width = 7.18, height = 7.08, units = 'in')
+  
+} 
 heatmap_img = readPNG("plots_AgingMice/png_for_plots/heatmap_75_normalized_clustered.png")
 heatmap_75 <- ggplot() +
   background_image(heatmap_img) +
@@ -103,11 +107,11 @@ sample_cor_withRequants <-calculate_sample_corr_distr(data_matrix = normalized_m
                                              repeated_samples = replicate_filenames,
                                              sample_id_col = 'FullRunName',
                                              batch_col = 'MS_batch')
-sample_cor_withRequants = sample_cor_withRequants %>% 
+sample_cor_withRequants = sample_cor_withRequants %>%
   mutate(batch_replicate_upd = ifelse(replicate, 'replicates', 
-                                      ifelse(batch_the_same, 'same batch', 'different batches')))
+                                      ifelse(batch_the_same, 'same\nbatch', 'different\nbatches')))
 sample_cor_withRequants$batch_replicate_upd = factor(sample_cor_withRequants$batch_replicate_upd,
-                                                   levels = c('different batches', 'same batch', 'replicates'))
+                                                   levels = c('different\nbatches', 'same\nbatch', 'replicates'))
 sample_cor_withRequants$Step = 'With requants'
 
 sample_cor_noRequants <-calculate_sample_corr_distr(data_matrix = normalized_matrix_AgingMice, 
@@ -115,11 +119,11 @@ sample_cor_noRequants <-calculate_sample_corr_distr(data_matrix = normalized_mat
                                                       repeated_samples = replicate_filenames,
                                                       sample_id_col = 'FullRunName',
                                                       batch_col = 'MS_batch')
-sample_cor_noRequants = sample_cor_noRequants %>% 
+sample_cor_noRequants = sample_cor_noRequants %>%
   mutate(batch_replicate_upd = ifelse(replicate, 'replicates', 
-                                      ifelse(batch_the_same, 'same batch', 'different batches')))
+                                      ifelse(batch_the_same, 'same\nbatch', 'different\nbatches')))
 sample_cor_noRequants$batch_replicate_upd = factor(sample_cor_noRequants$batch_replicate_upd,
-                                            levels = c('different batches', 'same batch', 'replicates'))
+                                            levels = c('different\nbatches', 'same\nbatch', 'replicates'))
 sample_cor_noRequants$Step = 'No requants'
 sample_cor_selected = rbind(sample_cor_noRequants, sample_cor_withRequants)
 sample_cor_selected$Step = factor(sample_cor_selected$Step,
@@ -130,10 +134,10 @@ corr_distr <- plot_sample_corr_distribution.corrDF(sample_cor_selected,
                                                    plot_param = 'batch_replicate_upd')+ 
   annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, size = 1.2)+
   theme(axis.title.x = element_blank(),
-        strip.text = element_text(face = 'bold', size = 14),
+        strip.text = element_text(face = 'bold', size = 16),
         strip.background=element_rect(color = NA,  fill=NA),
         panel.spacing = unit(9, "lines"),
-        axis.text.x = element_text(angle = 45,hjust=1, size = 10),
+        axis.text.x = element_text(angle = 45,hjust=1, size = 13),
         plot.margin = margin(t=.5, l=0, 
                              r=0, b=1, unit = "cm"))
 
@@ -141,7 +145,7 @@ corr_distr <- plot_sample_corr_distribution.corrDF(sample_cor_selected,
 #combine together
 panel_B_C = ggarrange(corr_heatmap+ 
                         theme(plot.margin = margin(t = .5, 
-                                                   r=1, b = .75, unit = "cm")), 
+                                                   r=1, b = .25, unit = "cm")), 
                       corr_distr + 
                         theme(plot.margin = margin(t = .5, 
                                                    r=3.4, b = 1, unit = "cm")), 
@@ -152,9 +156,10 @@ panel_B_C = ggarrange(corr_heatmap+
                              r=0, b=0, unit = "cm"))
 
 
-gg_Fig4 = ggarrange(heatmap_75_gg, 
+gg_Fig4 = ggarrange(heatmap_75_gg+ 
+                      theme(plot.margin = margin(r=2, unit = "cm")), 
                     panel_B_C,
           ncol = 2, nrow = 1, widths = c(2.35, 3))
 ggsave(gg_Fig4, 
-       filename = 'plots_AgingMice/Fig4_missing_values.pdf', 
-       dev = cairo_pdf, width = 17.79, height = 8.66)
+       filename = 'plots_AgingMice/Fig4_missing_values1.pdf', 
+       dev = cairo_pdf, width = 17.79, height = 8.96)
